9dbfa77fd7495470923262b23bb8a58e8c19245d
cofster <pcoffey2@gmail.com>
2016-10-18 05:38:27 +0000

    User Model Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-
-  get 'users/index'
-
-  get 'users/edit'
-
-  get 'users/profile'
-
-  get 'users/matches'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users do
+    member do
+      get 'profile'
+      get 'matches'
+  end
 end
